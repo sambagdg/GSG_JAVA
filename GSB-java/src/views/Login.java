f480f65c2f4f28954b010b68f188184647816d9d
sambagdg <103249282+sambagdg@users.noreply.github.com>
2023-04-20 22:57:47 +0200

    Add files via upload

---
+++ GSB-java/src/views/Login.java
@@ -0,0 +1,19 @@
+package views;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Login {
+    private JTextField loginTxt;
+    private JTextField mdpTxt;
+
+    public Login() {
+        mdpTxt.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                
+            }
+        });
+    }
+}
